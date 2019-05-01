6ee2c2992d1fdb2767ab9ca16903838c5ea3da3d
Andrea Castillo <acarolinecastillo@gmail.com>
2019-04-30 20:07:15 -0400

    Logout y mejoras de acceso a home.html y book.html

--- application.py
+++ application.py
@@ -24,7 +24,11 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template('login.html')
+
+    if session["user_id"] is None and session["user_name"] is None:
+        return render_template('login.html')
+    else:
+        return render_template('home.html')
     # return "Project 1: TODO"
 
 @app.route("/login", methods=['POST'])
@@ -49,7 +53,14 @@ def login():
         show_alert_wrong_pass = 1
         return render_template('login.html', user=user_row, show_alert_wrong_pass=show_alert_wrong_pass)
 
-    return
+@app.route("/logout")
+def logout():
+
+    session.pop('user_id', None)
+    session.pop('user_name', None)
+
+    return render_template('login.html')
+    # return "Project 1: TODO"
 
 @app.route("/registro", methods=['POST'])
 def registro():
@@ -68,38 +79,48 @@ def registro():
 @app.route("/home", methods=['GET', 'POST'])
 def home():
 
-    buscar = request.form.get("buscar")
+    if session.get("user_id") is None and session.get("user_name") is None:
+        return render_template('login.html')
+    else:
+        buscar = request.form.get("buscar")
 
-    # libros = db.execute("SELECT * FROM libro WHERE isbn LIKE {} OR title LIKE {}".format("'%" + buscar + "%'", "'%" + buscar + "%'")).fetchall()
-    libros = db.execute("SELECT l.id, l.isbn, l.title, l.year, a.name FROM libro l, autor a WHERE l.autor_id = a.id and \
-                            ( UPPER(l.isbn) LIKE UPPER('%{}%') OR UPPER(l.title) LIKE UPPER('%{}%') OR UPPER(a.name) LiKE UPPER('%{}%') )"
-                            .format(buscar, buscar, buscar)).fetchall()
+        if buscar is not None:
 
-    return render_template('home.html', libros=libros, buscar=buscar)
+            # libros = db.execute("SELECT * FROM libro WHERE isbn LIKE {} OR title LIKE {}".format("'%" + buscar + "%'", "'%" + buscar + "%'")).fetchall()
+            libros = db.execute("SELECT l.id, l.isbn, l.title, l.year, a.name FROM libro l, autor a WHERE l.autor_id = a.id and \
+                                    ( UPPER(l.isbn) LIKE UPPER('%{}%') OR UPPER(l.title) LIKE UPPER('%{}%') OR UPPER(a.name) LiKE UPPER('%{}%') )"
+                                    .format(buscar, buscar, buscar)).fetchall()
+
+            return render_template('home.html', libros=libros, buscar=buscar)
+
+        return render_template('home.html')
 
 @app.route("/books/<int:book_id>", methods=['GET', 'POST'])
 def book(book_id):
 
-    usuario_review_row = db.execute("SELECT * FROM review WHERE usuario_id = :user_id and libro_id = :libro_id", {"user_id": session["user_id"], "libro_id": book_id}).fetchone()
-
-    if usuario_review_row is not None:
-        show_alert_usuario_review = 1
+    if session.get("user_id") is None and session.get("user_name") is None:
+        return render_template('login.html')
     else:
-        show_alert_usuario_review = 0
+        usuario_review_row = db.execute("SELECT * FROM review WHERE usuario_id = :user_id and libro_id = :libro_id", {"user_id": session["user_id"], "libro_id": book_id}).fetchone()
 
-    if request.method == 'POST':
-
-        if show_alert_usuario_review is 0:
-            detail = request.form.get("detail")
-            stars = request.form.get("stars")
-
-            db.execute("INSERT INTO review (detail, stars, libro_id, usuario_id) VALUES (:detail, :stars, :libro_id, :usuario_id)",
-                            {"detail": detail, "stars": stars, "libro_id": book_id, "usuario_id": session["user_id"]})
-            db.commit()
-
+        if usuario_review_row is not None:
             show_alert_usuario_review = 1
+        else:
+            show_alert_usuario_review = 0
 
-    libro = db.execute("SELECT l.id, l.isbn, l.title, l.year, a.name FROM libro l, autor a WHERE l.autor_id = a.id and l.id = :id", {"id": book_id}).fetchone()
-    reviews = db.execute("SELECT r.*, u.user_name FROM review r, usuario u WHERE r.libro_id = :id and r.usuario_id = u.id", {"id": book_id}).fetchall()
+        if request.method == 'POST':
 
-    return render_template('book.html', libro=libro, reviews=reviews, show_alert_usuario_review=show_alert_usuario_review)
+            if show_alert_usuario_review is 0:
+                detail = request.form.get("detail")
+                stars = request.form.get("stars")
+
+                db.execute("INSERT INTO review (detail, stars, libro_id, usuario_id) VALUES (:detail, :stars, :libro_id, :usuario_id)",
+                                {"detail": detail, "stars": stars, "libro_id": book_id, "usuario_id": session["user_id"]})
+                db.commit()
+
+                show_alert_usuario_review = 1
+
+        libro = db.execute("SELECT l.id, l.isbn, l.title, l.year, a.name FROM libro l, autor a WHERE l.autor_id = a.id and l.id = :id", {"id": book_id}).fetchone()
+        reviews = db.execute("SELECT r.*, u.user_name FROM review r, usuario u WHERE r.libro_id = :id and r.usuario_id = u.id", {"id": book_id}).fetchall()
+
+        return render_template('book.html', libro=libro, reviews=reviews, show_alert_usuario_review=show_alert_usuario_review)
